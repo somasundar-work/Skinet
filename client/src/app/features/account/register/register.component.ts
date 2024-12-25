2e331054f637881a2a34ec16c7ea8afa587da43e
Somasundar <somausundar.work@outlook.com>
2024-12-25 23:18:31 +0530

    registration completed

--- client/src/app/features/account/register/register.component.ts
+++ client/src/app/features/account/register/register.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from '../../../core/services/account.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import {
+  MatError,
+  MatFormFieldModule,
+  MatLabel,
+} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { SnackbarService } from '../../../core/services/snackbar.service';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -18,6 +24,7 @@ import { MatInputModule } from '@angular/material/input';
     MatInputModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatError,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
@@ -26,10 +33,29 @@ export class RegisterComponent implements OnInit {
   private fb = inject(FormBuilder);
   private accountService = inject(AccountService);
   private router = inject(Router);
+  private snack = inject(SnackbarService);
+  validationErrors?: string[];
+
+  registerForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
   ngOnInit(): void {
     if (this.accountService.currentUser()) {
       this.router.navigateByUrl('/shop');
     }
+  }
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => {
+        this.snack.success('Registration successful - you can now login');
+        this.router.navigateByUrl('/account/login');
+      },
+      error: (errors) => (this.validationErrors = errors),
+    });
   }
 }
